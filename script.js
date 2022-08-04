c6a87be0d1e2aea6228aba4120d82c7508ab585e
Guilherme <guilhermeisk@hotmail.com>
2022-08-04 12:04:30 -0300

    Exercicio 7

---
+++ script.js
@@ -0,0 +1,21 @@
+// const botao=document.getElementById('numeros')
+// let pai=document.body
+// let filho=document.createElement('span')
+// function media(){
+//   const caixa= document.getElementById('focado')
+//   let mensagem=caixa.value
+//   console.log(mensagem)
+//   if(mensagem>=0 && mensagem<10){
+//     filho.innerText='Insuficiente '
+//     pai.appendChild(filho)
+//   } else if(mensagem>=10 && mensagem<14){
+//     filho.innerText='Bom '
+//     pai.appendChild(filho)
+//   } else if(mensagem>=14){
+//     filho.innerText='Muito Bom '
+//     pai.appendChild(filho)
+//   }
+// }
+
+// botao.addEventListener('click',media)
+
